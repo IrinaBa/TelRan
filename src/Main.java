5659c87fa0b9d7c1e04f8e99641c0df583feb9f6
Iryna Bartz <1208ein@mail.ru>
2022-06-23 21:07:58 +0200

    Change name

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    // Дан List<String>. Написать метод, который возвращает строку, которая есть результат объединения всех строк исходного листа через пробел.
+    //Например: {“I”, “do”, “my”, ”homework”} -> “I do my homework”.
+    public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("I");
+        stringList.add("do");
+        stringList.add("my");
+        stringList.add("homeworkkkkkkkk");
+        System.out.println(stringList);
+        System.out.println(toOneString(stringList));
+    }
+    public static String toOneString(List<String> stringList) {
+        String res = "";
+        if (stringList == null || stringList.isEmpty()) {
+            return res;
+        }
+        for (int i = 0; i < stringList.size(); i++) {
+            res += stringList.get(i) + " ";
+        }
+        return res;
+    }
+}
+
+
